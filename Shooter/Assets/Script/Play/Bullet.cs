dd8b84ca99d84d38f5deda8482104ca9a7ace120
kiennt261193 <kiennt@sandinh.net>
2019-11-14 17:55:34 +0700

    bullet

---
+++ Shooter/Assets/Script/Play/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rid;
+    private void OnEnable()
+    {
+        rid.velocity = transform.forward;
+    }
+}
