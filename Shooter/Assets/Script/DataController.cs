de5469bf42ff67dcf84c8cf6849d0f1c8944dd41
kiennt261193 <kiennt@sandinh.net>
2020-03-11 16:13:07 +0700

    log debugg

--- Shooter/Assets/Script/DataController.cs
+++ Shooter/Assets/Script/DataController.cs
@@ -245,7 +245,7 @@ public class DataController : MonoBehaviour
         if (loadData)
             return;
 
-      //  Debug.unityLogger.logEnabled = false;
+        Debug.unityLogger.logEnabled = false;
         LoadData();
         DataUtils.FillEquipmentData();
         loadData = true;
