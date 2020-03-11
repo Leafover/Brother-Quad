2612786bc93e4d894815f09598e9e1bdf5511046
kiennt261193 <kiennt@sandinh.net>
2020-03-11 11:16:39 +0700

    fix speed loading

--- Shooter/Assets/Script/Play/Menu/Loading.cs
+++ Shooter/Assets/Script/Play/Menu/Loading.cs
@@ -44,7 +44,7 @@ public class Loading : MonoBehaviour
 
             pos = fill.anchoredPosition;
             size = fill.sizeDelta;
-            size.x += Time.deltaTime * 250;
+            size.x += Time.deltaTime * 300;
             size.y = fill.sizeDelta.y;
             pos.x = size.x / 2;
             pos.y = fill.anchoredPosition.y;
