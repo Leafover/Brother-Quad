ad141928dcf2566b76d8cdf30541084b1e1c76d9
TranDua <duatv@sandinh.net>
2020-04-28 15:20:27 +0700

    fix scene_

--- Shooter/Assets/Script/MainMenu/Popup/PopupStarterPack.cs
+++ Shooter/Assets/Script/MainMenu/Popup/PopupStarterPack.cs
@@ -21,19 +21,26 @@ public class PopupStarterPack : MonoBehaviour
 
     void Start()
     {
-        txtPrice.text = GameIAPManager.GetPriceByID(DataUtils.P_STARTER_PACK);
         InitWeaponInfo();
+        if(GameIAPManager.GetPriceByID(DataUtils.P_STARTER_PACK) != null)
+            txtPrice.text = GameIAPManager.GetPriceByID(DataUtils.P_STARTER_PACK);
     }
 
-
+    private void ShowLog(string mess)
+    {
+        Debug.LogError("TAGG: " + mess);
+    }
     private void InitWeaponInfo()
     {
         var _wp = DataController.instance.allWeapon[6].weaponList[0];
+
+        ShowLog(_wp == null ? "WP NULL" : "WP NOT NULLL");
+        ShowLog("ItemName: " + _wp.NAME + " vs " + _wp.DmgValue[0] + " vs " + _wp.AtksecValue[0] + " vs " + _wp.CritRateValue[0]);
         tmpWPName.text = "<color=#5DADE2>" + _wp.NAME+"</color>";
-        txtDmg.text = DataUtils.GetRealFloat(_wp.DmgValue[0]); //_wp.DmgValue[0].ToString("0.##");
-        txtFireRate.text = DataUtils.GetRealFloat(_wp.AtksecValue[0]);
-        txtCritRate.text = DataUtils.GetRealFloat(_wp.CritRateValue[0]);
-        txtCritDmg.text = DataUtils.GetRealFloat(_wp.CritDmgValue[0]);
+        txtDmg.text = DataUtils.GetRealFloat(_wp.DmgValue[0]*10); //_wp.DmgValue[0].ToString("0.##");
+        txtFireRate.text = DataUtils.GetRealFloat(_wp.AtksecValue[0]) + "s";
+        txtCritRate.text = DataUtils.GetRealFloat(_wp.CritRateValue[0])+"%";
+        txtCritDmg.text = DataUtils.GetRealFloat(_wp.CritDmgValue[0])+"%";
         txtRange.text = DataUtils.GetRealFloat(_wp.AtkRangeValue[0]);
         txtMagazine.text = DataUtils.GetRealFloat(_wp.MagazineValue[0]);
     }
